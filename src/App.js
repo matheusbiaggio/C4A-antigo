db5bf9f2489b00c90e73c2d6e1dd095661dd053f
Matheus Biaggio <matheusbiaggio@gmail.com>
2023-06-07 18:50:41 -0300

    Implementação do navbar sem css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from "react";
+import {
+  BrowserRouter, Routes, Route,
+} from 'react-router-dom';
+import './App.css';
+import Home from "./pages/Home";
 
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={ <Home /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Logo from '../images/Logo.svg';
+import ExitIcon from '../images/Exit.svg';
+import FunctionIcon from '../images/Function.svg';
+import InfoIcon from '../images/Info.svg';
+import MenuIcon from '../images/Menu.svg';
+import ShieldIcon from '../images/Shield.svg';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div>
+        <img src={ Logo } alt="Logo" />
+        <div>
+          <button>
+            <img src={ FunctionIcon } alt="FunctionIcon" />
+          </button>
+          <button>
+            <img src={ ShieldIcon } alt="ShieldIcon" />
+          </button>
+          <button>
+            <img src={ InfoIcon } alt="InfoIcon" />
+          </button>
+          <button>
+            <img src={ ExitIcon } alt="ExitIcon" />
+          </button>
+        </div>
+      </div>
+      <button>
+        <img src={ MenuIcon } alt="MenuIcon" />
+      </button>
+    </nav>
+  )
+}
